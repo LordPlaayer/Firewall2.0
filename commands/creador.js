e3900579a18a6810795478168379dbbb44e5ea90
PandaBoyYT <37066611+PandaBoyYT@users.noreply.github.com>
2019-01-03 19:56:05 -0300

    Update creador.js

--- commands/creador.js
+++ commands/creador.js
@@ -1,5 +1,19 @@
-client.on("message", (message) !! {
-  if (message.content.startsWith("creador")) {
-    message.channel.send("Mi creador es **LordPlaayer#6030**");
-  } else
-});
+const Discord = require("discord.js");
+
+exports.run = (client, message, args, level) => {
+  message.delete()
+
+  const embed = new Discord.RichEmbed()
+  .setTitle("Dato:")
+  .setColor("#5599ff")
+  .setDescription(`Mi creador es **LordPlaayer#6030**`)
+  .addField("", "", true)
+  .setFooter(`${message.author.username}`, `${message.author.avatarURL}`)
+
+  message.channel.send({embed})
+  .then(msg => {
+    msg.react('👍')
+  })
+  .catch(() => console.error('Emoji failed to react.'));
+
+}
