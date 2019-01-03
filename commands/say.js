346af1404d2e1b096075dc976b43858ca686616f
LordPlaayer <46354379+LordPlaayer@users.noreply.github.com>
2019-01-03 23:32:22 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-      if(!message.member.hasPermission("ADMINISTRATOR")) return      message.channel.send("Error: Necesitas los permisos de Administrador");;
+      if(!message.member.hasPermission("ADMINISTRATOR")) return      message.channel.send("**ERROR** Lo siento, no eres STAFF de Fireware");;
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
