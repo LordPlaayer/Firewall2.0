5736416ae5e82963885e12b6786a27ed679f730e
LordPlaayer <46354379+LordPlaayer@users.noreply.github.com>
2019-01-05 21:02:34 +0100

    Update estadofivem.js

--- commands/estadofivem.js
+++ commands/estadofivem.js
@@ -4,10 +4,9 @@ exports.run = (client, message, args, level) => {
   message.delete()
 
   const embed = new Discord.RichEmbed()
-  .setTitle("FiveM:")
-  .setColor("#00ECFF")
-  .setDescription(`https://trello.com/b/k8pq0H4F/roleplayworld-roadmap**`)
-  .setFooter(`${message.author.username}`, `${message.author.avatarURL}`)
+  .setTitle("Estado del Servidor:")
+  .setColor("#00E8FF")
+  .setDescription(`**https://trello.com/b/k8pq0H4F/fivem-rp-firewall-games**`)
 
   message.channel.send({embed})
   .then(msg => {
