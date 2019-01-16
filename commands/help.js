cde8b12e32e741706cc6bfef7e71c584d0a2859c
LordPlaayer <46354379+LordPlaayer@users.noreply.github.com>
2019-01-16 22:49:46 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,14 @@ exports.run = (client, message, args, level) => {
   message.delete()
 
   const embed = new Discord.RichEmbed()
-  .setColor("#00ECFF")
-  .setDescription
-(`
+  .setTitle("Estado del Servidor:")
+  .setColor("#00E8FF")
+  .setDescription(`
 **Mis Comandos**
 !!creador
 !!fundadores
 !!estadofivem
-!!avances
-`)
+!!avances`)
+ message.channel.send({embed})
+})
 }
