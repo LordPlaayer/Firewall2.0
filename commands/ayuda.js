8a4e32cb7069f838d381fce978dba0b1aef3b585
LordPlaayer <46354379+LordPlaayer@users.noreply.github.com>
2019-01-19 14:50:03 +0100

    Update ayuda.js

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -6,16 +6,15 @@ exports.run = (client, message, args, level) => {
   const embed = new Discord.RichEmbed()
   .setTitle("Comandos de Firewall Games:")
   .addField("Mis Comandos:\n" +
-                                  "!!creador \n" +
-                                  "**Te enseño quien es mi creador** \n" +
-                                  "!!avances \n" +
-                                  "**Aqui salen mis actualizaciones (No disponible)** \n" , true)
-  .addField("!!fundadores\n" +
-                                  "**Te enseño quienes son los CEO de Firewall Games**\n" +
-                                  "!!estadofivem \n" +
-                                  "**Te enseño el Trello de como va el servidor**\n" +
+                                  "**!!creador** \n" +
+                                  "Te enseño quien es mi creador \n" +
+                                  "**!!avances** \n" +
+                                  "Aqui salen mis actualizaciones (No disponible) \n" , true)
+  .addField("**!!fundadores**\n" +
+                                  "Te enseño quienes son los CEO de Firewall Games\n" +
+                                  "**!!estadofivem** \n" +
+                                  "**Te enseño el Trello de como va el servidor**\n", true)
                                   "Otros Bots\n" +
-                                  "!!help \n", true)
   .setColor("#FFB600")
   message.channel.send({embed})
 
